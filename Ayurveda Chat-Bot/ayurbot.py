ce9d1368e25806e95e1004b37780498919a62e43
DevG10 <devgovindani8343@gmail.com>
2024-01-30 13:00:54 +0530

    Update Bot

--- Ayurveda Chat-Bot/ayurbot.py
+++ Ayurveda Chat-Bot/ayurbot.py
@@ -6,13 +6,14 @@ import streamlit as st
 from streamlit_chat import message
 from sentence_transformers import SentenceTransformer
 from dotenv import load_dotenv
-import pinecone
+from pinecone.grpc import PineconeGRPC
 from utils import *
 from openai import OpenAI
 import os
 
-
-
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+load_dotenv()
+client = OpenAI()
 st.set_page_config(page_title='Ayurveda Chatbot', page_icon=':herb:')
 st.header('Ayurvedic chatbot for a healthy routine :herb:')
 with st.sidebar:
@@ -23,11 +24,7 @@ with st.sidebar:
         ''')
     st.write('Made with ❤️ by [Team Ayurmarg](https://www.youtube.com/watch?v=6_fYrOHm7QE&t=13s)')
 
-load_dotenv("D:\\Computer_Programming\\Python\\ayurveda_model\\.env")
 
-client = OpenAI(
-    api_key = os.environ['OPENAI_API_KEY']
-)
 
 if 'responses' not in st.session_state:
     st.session_state['responses'] = ['How can I help you?']
@@ -39,7 +36,7 @@ llm = ChatOpenAI(model_name = "gpt-3.5-turbo")
 if 'buffer_mmeory' not in st.session_state:
     st.session_state.buffer_mmeory = ConversationBufferWindowMemory(k = 3, return_messages = True)
     
-system_msg_template = SystemMessagePromptTemplate.from_template(template = """Answer the question as truthfully as possible using the provided context, and if the answer is not contained within the text below, say 'I dont know'""")
+system_msg_template = SystemMessagePromptTemplate.from_template(template = """Answer the question as truthfully as possible using the provided context, and if the answer is not contained within the text below, say 'As an Ayurvedic assistant, I can only answer questions related to Ayurveda.' else if the user is askign for greeting messages you can greet him too nicely""")
 
 human_msg_template = HumanMessagePromptTemplate.from_template(template = "{input}")
 
@@ -53,12 +50,9 @@ text_container = st.container()
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
-pinecone.init(
-    api_key = "e3600f56-6371-4ac1-9f29-fa22ea086c42",
-    environment =  "gcp-starter"
-)
+pc = PineconeGRPC(api_key=os.getenv("PINECONE_API_KEY"))
 
-index = pinecone.Index('ayurveda-chatbot')
+index = pc.Index('ayurveda-chatbot')
 
 
 def find_match(input):
@@ -67,16 +61,14 @@ def find_match(input):
     return result['matches'][0]['metadata']['text']+"\n"+result['matches'][1]['metadata']['text']
 
 
-
 def query_refiner(conversation, query):
-    client = OpenAI()
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=[{"role": "system", "content": "You are a helpful assistant providing information only on ayurveda related topics"},
+        messages=[{"role": "system", "content": "You are a highly helpful assistant providing information only on ayurveda related topics and you can answer the in general questions like hi, hello and other type of greetings."},
         {"role": "user", "content": f"Context: {conversation}"},
         {"role": "assistant", "content": f"Query: {query}"}],
 
-        temperature=0.5,
+        temperature=0.1,
         max_tokens=256,
         top_p=1,
         frequency_penalty=0,
@@ -91,20 +83,21 @@ def get_conversation_string():
         conversation_string += "Bot: "+ st.session_state['responses'][i+1] + "\n"
     return conversation_string
 
-
+if 'user_input' not in st.session_state:
+    st.session_state.user_input = ""
 
 with text_container:
-    query = st.text_input("How may I help you? ", key = "input")
+    query = st.text_input("How may I help you? ", key="input")
     if query:
         with st.spinner("Generating the response..."):
             conversation_string = get_conversation_string()
             st.code(conversation_string)
             refined_query = query_refiner(conversation_string, query)
             context = find_match(refined_query)
-            response = conversation.predict(input = f"Context: \n {context} \n\n Query: \n {query}")
+            response = conversation.predict(input=f"Context: \n {context} \n\n Query: \n {query}")
             st.session_state.requests.append(query)
             st.session_state.responses.append(response)
-
+            st.session_state.user_input = ""
 
 with response_container:
     if st.session_state['responses']:
