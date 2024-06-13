28c8e8c175615f8e335c3b70af1d8d653a5481fe
Dev Govindani <115708950+DevG10@users.noreply.github.com>
2024-06-14 01:24:06 +0530

    Update front_end.py

--- RAGxGemini/front_end.py
+++ RAGxGemini/front_end.py
@@ -5,9 +5,9 @@ st.set_page_config(page_title="RAG Demo", page_icon="🤖")
 
 st.title("RAG Demo using Streamlit and Google's Gemini Pro")
 
-input = st.text_input("Enter your question here", key="user_input")
+get_question = st.text_input("Enter your question here", key="user_input")
 
 if st.button("Ask"):
-    with st.spinner("Loading..."):
-        answer = user_input(input)
+    with st.spinner("Generating Answer"):
+        answer = user_input(get_question)
     st.write("Answer: ", answer['output_text'])
